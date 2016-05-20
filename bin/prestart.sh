b9372e17c44e0d2cb731cfc173dfad2a50f5ab47
Miguel Terron <miguel.a.terron@gmail.com>
2016-05-20 17:57:36 +1200

    prestart cleanup

--- bin/prestart.sh
+++ bin/prestart.sh
@@ -8,11 +8,12 @@ loge() {
 
 # update elasticsearch URL configuration
 configure() {    
+	ES=$(curl -Ls --fail "${CONSUL}/v1/health/service/elasticsearch-data?passing" | jq -e -r '[.[].Service.Address]' | tr -d ' \r\n' | sed 's/",/:9200",/g'| sed 's/"]/:9200"]/')
+	log "Elasticsearch data node(s): $ES"
     REPLACEMENT=$(printf 's/hosts\s*=> \[.*\]/hosts\t=>%s/' "$ES")
 	sed -i "$REPLACEMENT" /opt/logstash/config/logstash.conf
 }
 ###################################################################################################
-
 # Wait till Elasticsearch is available
 log "Waiting for Elasticsearch data node..."
 until (curl -Ls --fail "${CONSUL}/v1/health/service/elasticsearch-data?passing" | jq -e -r '.[0].Service.Address' >/dev/null); do
@@ -20,8 +21,6 @@ until (curl -Ls --fail "${CONSUL}/v1/health/service/elasticsearch-data?passing" 
 done
 
 log "Elasticsearch is now available, configuring Logstash"
-ES=$(curl -Ls --fail "${CONSUL}/v1/health/service/elasticsearch-data?passing" | jq -e -r '[.[].Service.Address]' | tr -d ' \r\n' | sed 's/",/:9200",/g'| sed 's/"]/:9200"]/')
-log "Elasticsearch data node(s): $ES"
 configure
 # if logstash -t -f /opt/logstash/config/logstash.conf; then 
 #     exit 0
@@ -30,5 +29,3 @@ configure
 #     exit 1
 # fi
 exit 0
-
-
