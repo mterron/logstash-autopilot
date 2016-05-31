f4fa881b797aec734c04b831ff895b0572044609
Miguel Terron <miguel.a.terron@gmail.com>
2016-06-01 10:17:57 +1200

    Corrected Consul environment variable (CONSUL -> CONSUL_HTTP_ADDR)

--- bin/prestart.sh
+++ bin/prestart.sh
@@ -8,22 +8,22 @@ loge() {
 
 # update logstash URL configuration
 configure() {    
-	ES=$(curl -Ls --fail "${CONSUL}/v1/health/service/elasticsearch-data?passing" | jq -c -e -r '[.[].Service.Address+":9200"]')
+	ES=$(curl -Ls --fail "${CONSUL_HTTP_ADDR}/v1/health/service/elasticsearch-data?passing" | jq -c -e -r '[.[].Service.Address+":9200"]')
 	log "Elasticsearch data node(s): $ES"
     REPLACEMENT=$(printf 's/hosts\s*=> \[.*\]/hosts\t=>%s/' "$ES")
 	sed -i "$REPLACEMENT" /opt/logstash/config/logstash.conf
 }
 #------------------------------------------------------------------------------
-# Check that CONSUL environment variable exists
-if [[ -z ${CONSUL} ]]; then
-    loge "Missing CONSUL environment variable"
+# Check that CONSUL_HTTP_ADDR environment variable exists
+if [[ -z ${CONSUL_HTTP_ADDR} ]]; then
+    loge "Missing CONSUL_HTTP_ADDR environment variable"
     exit 1
 fi
 
 # Wait up to 2 minutes for Consul to be available
 log "Waiting for Consul availability..."
 n=0
-until [ $n -ge 120 ]||(curl -fsL --connect-timeout 1 "${CONSUL}/v1/status/leader" &> /dev/null); do
+until [ $n -ge 120 ]||(curl -fsL --connect-timeout 1 "${CONSUL_HTTP_ADDR}/v1/status/leader" &> /dev/null); do
     sleep 2
     n=$((n+2))
 done
@@ -36,7 +36,7 @@ fi
 log "Consul is now available [${n}s], starting up Logstash"
 # Wait till Elasticsearch is available
 log "Waiting for Elasticsearch data node..."
-until (curl -Ls --fail "${CONSUL}/v1/health/service/elasticsearch-data?passing" | jq -e -r '.[0].Service.Address' >/dev/null); do
+until (curl -Ls --fail "${CONSUL_HTTP_ADDR}/v1/health/service/elasticsearch-data?passing" | jq -e -r '.[0].Service.Address' >/dev/null); do
     sleep 2
 done
 
